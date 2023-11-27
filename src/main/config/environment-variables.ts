24ab7336129645dc8f007823c56a4b297ce013ec
Marcos Uchoa <marcospacheco10111@gmail.com>
2023-11-27 01:16:13 -0300

    update config with firebase

--- src/main/config/environment-variables.ts
+++ src/main/config/environment-variables.ts
@@ -12,3 +12,12 @@ export const BEARER_TOKEN = process.env.BEARER_TOKEN;
 export const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 
 export const WHATSAPP_ENABLED = process.env.WHATSAPP_ENABLED?.toLowerCase() in {"true": true, "1": true };
+
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APIID
+}
